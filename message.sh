b6481f48b669081d1d6525b7d2fd1f3ebb838b5a
Jingchang Shi <jingchangshi@gmail.com>
2020-10-08 11:10:29 +0800

    refactor: update bash script

---
+++ message.sh
@@ -0,0 +1,7 @@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+echo -e "${RED}If you want to know the CPU load of all compute nodes, use the following cmd,${NC}"
+echo -e "${BLUE}SCM_Client${NC}"
+echo -e "${RED}NOTE: SCM_Client can only be run on the master node.${NC}"
+
--- root_kill.sh
+++ root_kill.sh
@@ -1,1 +1,3 @@
-kill -9 $(cat $HOME/SCM_Server_$(hostname).pid)
+# kill -9 $(cat $HOME/SCM_Server_$(hostname).pid)
+ps aux | grep SCM_Server | awk '{print $2}' | while read i; do kill -9 $i; done
+
