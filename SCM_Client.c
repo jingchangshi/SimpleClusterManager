70ad79d86cd28d345b84fb0493571d82d4890deb
Jingchang Shi <jingchangshi@gmail.com>
2021-01-20 01:20:30 +0800

    feat: allow time out for connect

--- SCM_Client.c
+++ SCM_Client.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #include <pthread.h>
 
 #define YELLOW "\e[0;33m"
+#define BRIGHTYELLOW "\e[0;93m"
 #define RESET "\e[0m"
 
 void* collectInfo1Node(int* sockfd) {
@@ -24,12 +30,14 @@ void* collectInfo1Node(int* sockfd) {
 
 }
 
-int main(int argc , char *argv[])
-{
+int main(int argc , char *argv[]) {
 
   printf("Simple Cluster Manager (SCM) shows CPU loading of compute nodes.\n");
   printf("SCM is implemented by Jingchang Shi.\n");
   printf("Go to https://github.com/desperadoshi/SimpleClusterManager/issues for issues.\n");
+  printf(BRIGHTYELLOW);
+  printf("It is possible that due to the bad network, you have to wait for max 2 seconds of timeout.\n");
+  printf(RESET);
 
   int n_socket = 8;
   // create socket
@@ -46,27 +54,97 @@ int main(int argc , char *argv[])
   bzero(&info,sizeof(info));
   info.sin_family = PF_INET;
 
-  /* info.sin_addr.s_addr = inet_addr("127.0.0.1"); */
-
+  struct timeval tv;
+  socklen_t lon;
+  int valopt;
+  fd_set myset;
+  long arg;
   int err;
+  bool conn_ok;
   char ip_str[15] = {};
   int n_socket_conn = 0, n_socket_not_conn = 0;
   int socket_conn[n_socket];
   int socket_not_conn[n_socket];
   for(int i=0;i<n_socket;i++) {
+    // Set non-blocking socket
+    if ( (arg = fcntl(sockfd[i], F_GETFL, NULL)) < 0 ) {
+      fprintf(stderr, "Error fcntl(..., F_GETFL) (%s)\n", strerror(errno));
+      exit(0);
+    }
+    arg |= O_NONBLOCK; 
+    if ( fcntl(sockfd[i], F_SETFL, arg) < 0 ) {
+      fprintf(stderr, "Error fcntl(..., F_SETFL) (%s)\n", strerror(errno));
+      exit(0);
+    }
+    // Connect
     err = sprintf(ip_str, "192.168.1.%d", 101+i);
     info.sin_port = htons(8701+i);
     info.sin_addr.s_addr = inet_addr(ip_str);
     err = connect(sockfd[i],(struct sockaddr *)&info,sizeof(info));
-    if(err==-1){
-      /* printf("Connection error\n"); */
-      socket_not_conn[n_socket_not_conn] = i;
-      n_socket_not_conn += 1;
+    // Check the status
+    if(err<0){
+      if (errno == EINPROGRESS) {
+        /* fprintf(stderr, "EINPROGRESS in connect() - selecting\n"); */
+        do {
+          tv.tv_sec = 2; 
+          tv.tv_usec = 0; 
+          FD_ZERO(&myset); 
+          FD_SET(sockfd[i], &myset); 
+          err = select(sockfd[i]+1, NULL, &myset, NULL, &tv);
+          if (err < 0 && errno != EINTR) {
+            fprintf(stderr, "Error connecting %d - %s\n", errno, strerror(errno));
+            exit(0);
+          } else if (err > 0) {
+            // Socket selected for write
+            lon = sizeof(int);
+            if (getsockopt(sockfd[i], SOL_SOCKET, SO_ERROR, (void*)(&valopt), &lon) < 0) {
+              fprintf(stderr, "Error in getsockopt() %d - %s\n", errno, strerror(errno));
+              exit(0);
+            }
+            // Check the value returned
+            if (valopt) {
+              /* fprintf(stderr, "Error in delayed connection() %d - %s\n", valopt, strerror(valopt)); */
+              /* exit(0); */
+              conn_ok = false;
+              break;
+            }
+            conn_ok = true;
+            break;
+          } else {
+            // ??? Should I continue?
+            /* fprintf(stderr, "Timeout in select() - Cancelling!\n"); */
+            /* exit(0); */
+            conn_ok = false;
+            break;
+          }
+        } while (1);
+      } else {
+        /* fprintf(stderr, "Error connecting %d - %s\n", errno, strerror(errno)); */
+        /* exit(0); */
+        conn_ok = false;
+      }
     } else {
+      conn_ok = true;
+    }
+    //
+    if (conn_ok) {
       socket_conn[n_socket_conn] = i;
       n_socket_conn += 1;
+    } else {
+      socket_not_conn[n_socket_not_conn] = i;
+      n_socket_not_conn += 1;
     }
-  }
+    // Set to blocking mode again... 
+    if ( (arg = fcntl(sockfd[i], F_GETFL, NULL)) < 0 ) {
+      fprintf(stderr, "Error fcntl(..., F_GETFL) (%s)\n", strerror(errno));
+      exit(0);
+    }
+    arg &= (~O_NONBLOCK);
+    if( fcntl(sockfd[i], F_SETFL, arg) < 0) {
+      fprintf(stderr, "Error fcntl(..., F_SETFL) (%s)\n", strerror(errno));
+      exit(0);
+    }
+  } // for(int i=0;i<n_socket;i++)
 
   //Send a message to server
   /* Serial version */
@@ -121,6 +199,6 @@ int main(int argc , char *argv[])
   /* printf("close Socket\n"); */
   /* close(sockfd); */
   /* return 0; */
-}
+} // int main
 
 
